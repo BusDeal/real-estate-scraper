c6955fcdac88e141ece823081eceee8563a24f0c
Devesh <deveshucer@gmail.com>
2024-10-14 14:55:06 +0530

    adding timeit docs

--- utils/helpers.py
+++ utils/helpers.py
@@ -9,12 +9,30 @@ def clean_text(text: str) -> str:
 lgr = Logger(config.LOG_TO_FILE, config.LOG_FILE_NAME).get_logger()
 
 def timeit(func):
+    """
+    This is a wrapper for the timeit decorator. It records the start time of a function call, 
+    executes the function, records the end time, calculates the difference, and prints the time 
+    taken in milliseconds and seconds.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: A new function that wraps the original function and adds the timing feature.
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         """
         This is a wrapper for the timeit decorator. It records the start time of a function call, 
         executes the function, records the end time, calculates the difference, and prints the time 
         taken in milliseconds and seconds.
+
+        Args:
+            *args (tuple): The positional arguments to be passed to the function.
+            *kwargs (dict): The keyword arguments to be passed to the function.
+
+        Returns:
+            Any: The result of the function call.
         """
         start_time = time.time()  # Record the start time
         result = func(*args, **kwargs)  # Execute the function
